1f27b8b2358abbb0a89399751efe2e21102a325a
Kobina Koomson <georgeranch31@gmail.com>
2018-01-07 01:54:11 +0000

    innitial commmit

---
+++ v-dom.js
@@ -0,0 +1,33 @@
+/**
+ *
+ * @Author       Created by arch on 29/06/17 using 1/7/18.
+ * @Time         : 00:19
+ * @Copyright (C) 2018$
+ * @version 2.3.5
+ * Barge Studios Inc, The $ Authors
+ * <bargestd@gmail.com>
+ * <bumble.bee@bargestd.com>
+ *
+ * @licence      Licensed under the Barge Studios Eula
+ *  you may not use this file except in compliance with the License.
+ *
+ * You may obtain a copy of the License at
+ *     http://www.bargestudios.com/bumblebee/licence
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS-IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ *        \__/
+ *    \  (-_-)  /
+ *    \-( ___)-/
+ *     ( ____)
+ *   <-(____)->
+ *    \      /
+ * @fileOverview contains instruction[code] for creating $ this
+ *
+ * @requires {@link $}
+ *
+ */
